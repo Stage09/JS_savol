0bb5cbc95b4cecfbe27bcfd1620975166f33a541
Stage09 <ismoil281279@gmail.com>
2024-10-26 15:37:40 +0500

    finished

---
+++ main.js
@@ -0,0 +1,47 @@
+var question1 = "1. HTML sozni toliq yozilishi nima? -"
+var question2 = "2. HTML nima uchun kerak?  -"
+var question3 = "3. CSS da nima qilish mumkun?-"
+var question4 = "4. div nima?-"
+var question5 = "5. Heading nima?-"
+var question6 = "6. Class nima uchun kerak?-"
+var question7 = "7. Max-width da nima qilinadi?-"
+var question8 = "8. JavaSript nima?-"
+var question9 = "9. SVG uslubi haqida nima deya olasiz?-"
+var question10 = "10. Container nima degani-"
+var question11 = "11. CSS sozni toliq yozilishi noma-"
+var question12 = "12. Opocity nima degani-"
+var question13 = "13. Position:fixed ni qachon islatamiz -"
+var question14 = "14. Tailwind CSS nima degan-"
+var question15 = "15. a href = ni qachon ishlatamiz? -"
+
+var answer1 = prompt(question1)
+var answer2 = prompt(question2)
+var answer3 = prompt(question3)
+var answer4 = prompt(question4)
+var answer5 = prompt(question5)
+var answer6 = prompt(question6)
+var answer7 = prompt(question7)
+var answer8 = prompt(question8)
+var answer9 = prompt(question9)
+var answer10 = prompt(question10)
+var answer11 = prompt(question11)
+var answer12 = prompt(question12)
+var answer13 = prompt(question13)
+var answer14 = prompt(question14)
+var answer15 = prompt(question15)
+
+console.log(question1, answer1)
+console.log(question2, answer2)
+console.log(question3, answer3)
+console.log(question4, answer4)
+console.log(question5, answer5)
+console.log(question6, answer6)
+console.log(question7, answer7)
+console.log(question8, answer8)
+console.log(question9, answer9)
+console.log(question10, answer10)
+console.log(question11, answer11)
+console.log(question12, answer12)
+console.log(question13, answer13)
+console.log(question14, answer14)
+console.log(question15, answer15)
